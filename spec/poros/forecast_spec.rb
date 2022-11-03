1b9d105bd703e0258e3406160403b664cfb518f4
Kaelin Sleevi <105956031+KaelinSleevi@users.noreply.github.com>
2022-11-03 11:00:50 -0600

    Update forecast_spec.rb
    Remove 'pry'

--- spec/poros/forecast_spec.rb
+++ spec/poros/forecast_spec.rb
@@ -4,6 +4,5 @@ RSpec.describe 'Forecast POROS' do
   it 'returns a simplified weather forecast for the area', :vcr do
     # @zone = ZoneExample.new
     @forecast = ForecastFacade.daily_forecast(47.427, -121.418)
-    require 'pry'; binding.pry
   end
 end
