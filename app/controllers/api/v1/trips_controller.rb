e7c84a718aadaca4d35d6e5a053de0911892982d
Jake Kim <imjakekim@gmail.com>
2022-11-03 18:01:17 -0700

    finished testing for creating a trip

--- app/controllers/api/v1/trips_controller.rb
+++ app/controllers/api/v1/trips_controller.rb
@@ -11,6 +11,17 @@ class Api::V1::TripsController < ApplicationController
   end
 
   def create
-    
+    trip = Trip.new(trip_params)
+
+    if trip.save
+      render json: TripSerializer.new(trip)
+    else
+      render status: 404
+    end
+  end
+  
+private
+  def trip_params
+    params.permit(:name, :zone_id, :start_date, :description, :user_id)
   end
 end
