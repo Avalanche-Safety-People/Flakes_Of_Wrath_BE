bcc46bbbf8d98280e9127114f8f8cdb605cb73b5
Jake Kim <imjakekim@gmail.com>
2022-11-04 14:12:15 -0700

    added fake seed data to be seeded on heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+bob = User.create!(uid: "123", provider: "google", name: "bob", email: "bob@gmail.com", favorite_zone: 88)
+sally = User.create!(uid: "123", provider: "google", name: "sally", email: "sally@gmail.com", favorite_zone: 4)
+
+bob.emergency_contacts.create!(name: "mommy", phone_number: "1 800 588 2300")
+bob.emergency_contacts.create!(name: "Darth Vader", phone_number: "911")
+
+bob.trips.create!(name: "Adventure is out there", zone_id: 88, start_date: "2023-2-3", description: "very moist")
+bob.trips.create!(name: "Prom Night!", zone_id: 4, start_date: "2023-7-3", description: "#YOLO")
